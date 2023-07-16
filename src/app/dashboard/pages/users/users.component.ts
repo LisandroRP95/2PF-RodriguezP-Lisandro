97a7f42a7299df8a3031569439a5e2b89f724627
Lisandro Rodriguez <licharp41@gmail.com>
2023-07-16 16:28:01 -0300

    validaciones en formularios

--- src/app/dashboard/pages/users/users.component.ts
+++ src/app/dashboard/pages/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-users',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./users.component.scss']
 })
 export class UsersComponent {
+
+  nameControl = new FormControl(null, [Validators.required]);
+  surnameControl = new FormControl(null, [Validators.required]);
+  emailControl = new FormControl(null, [Validators.required]);
+  passwordControl = new FormControl(null, [Validators.required]);
+
+  userForm = new FormGroup({
+    name: this.nameControl,
+    surname: this.surnameControl,
+    email: this.emailControl,
+    password: this.passwordControl
+  });
+
+  onSubmit(): void{
+    alert(JSON.stringify(this.userForm.value))
+  }
 
 }
